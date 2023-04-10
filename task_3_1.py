ffd91c5550d757fc0c04b7d4c09dc3f8b44e090e
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-10 12:12:10 +0300

    Выполнено

--- task_3_1.py
+++ task_3_1.py
@@ -85,3 +85,37 @@ print(m)
 
 # Вывод количества строк и столбцов
 print("Количество строк:", m.get_rows_count(), "Количество столбцов:", m.get_cols_count())
+
+# Да, есть еще вариант через создания словарей
+class Table:
+    def __init__(self, colnames: list):
+        self.colnames = colnames
+        self.rows = []
+
+    def add_row(self, row: dict):
+        if any(colname not in tuple(row) for colname in self.colnames):  # хотя бы один элемент не соответствует
+            print('Такой колонки нет')
+        else:
+            self.rows.append(row)
+
+    def get_column(self, colname):
+        if (colname not in self.colnames):
+            print('Такой колонки нет')
+        return [r[colname] for r in self.rows]
+
+
+    def sum(self, colname: str):
+        if colname not in self.colnames:
+          print('Неправильный формат')
+        return sum([r[colname] for r in self.rows])
+
+
+
+table1 = Table([1, 2])
+table1.add_row({1: 10, 2: 20})
+table1.add_row({1: 20, 2: 400})
+table1.add_row({1: 30, 2: 600})
+table1.add_row({1: 40, 2: 800})
+print(table1.get_column(1))
+print(table1.get_column(2))
+print(table1.rows)
